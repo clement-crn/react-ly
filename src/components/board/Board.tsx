07087db11a94c7b2a04d2fe66f15feb2dc1a6f26
Clément <cironclement.pro@gmail.com>
2023-11-13 00:15:15 +0100

    Board prototype

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,8 +1,40 @@
 import React from "react"
+import Box from "../box/Box"
 import style from "./Board.module.css"
 
 function Board() {
-  return <div className={style.board_container}>je suis board</div>
+  function getDefaultBoxProps(index) {
+    return {
+      name: `Box ${index + 1}`,
+      colorFamily: "Default Color",
+      price: 10,
+    }
+  }
+
+  const boxesData = Array.from({ length: 10 * 10 }, (_, index) =>
+    getDefaultBoxProps(index)
+  )
+  return (
+    <div className={style.board_container}>
+      {Array.from({ length: 10 }, (_, rowIndex) => (
+        <div key={rowIndex} className={style.board_row}>
+          {Array.from({ length: 10 }, (_, colIndex) => {
+            const boxIndex = rowIndex * 10 + colIndex
+
+            return (
+              <div key={boxIndex} className={style.board_cell}>
+                {boxIndex < boxesData.length ? (
+                  <Box {...boxesData[boxIndex]} />
+                ) : (
+                  <div className={style.empty_space} />
+                )}
+              </div>
+            )
+          })}
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Board
--- src/components/box/Box.tsx
+++ src/components/box/Box.tsx
@@ -1,17 +1,17 @@
 import React from "react"
-
+import style from "./Box.module.css"
 interface BoxProps {
-  name: string
-  colorFamily: string
-  price: number
+  name?: string
+  colorFamily?: string
+  price?: number
 }
 
 function Box({ name, colorFamily, price }: BoxProps) {
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>Color Family: {colorFamily}</p>
-      <p>Price: ${price}</p>
+    <div className={style.box}>
+      <h3>{name}</h3>
+      Color Family: {colorFamily}
+      Price: ${price}
     </div>
   )
 }
