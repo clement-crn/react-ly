002eda52857175e4eb1816edf2a654862814a204
Clément <cironclement.pro@gmail.com>
2023-11-18 00:05:34 +0100

    init districtZone type

--- src/components/board/const.tsx
+++ src/components/board/const.tsx
@@ -1,6 +1,49 @@
-import { CornerZone } from "./types"
+import { CornerZone, DistrictZone } from "./types"
 
 export const cornerZone: CornerZone[] = [
+  {
+    id: 0,
+    name: "Départ",
+    coordinates: {
+      top: "86.5%",
+      left: "121.5%",
+      width: "18.5%",
+      height: "13%",
+    },
+  },
+  {
+    id: 1,
+    name: "Prison",
+    coordinates: {
+      top: "86.5%",
+      left: "0%",
+      width: "18.5%",
+      height: "13%",
+    },
+  },
+  {
+    id: 2,
+    name: "Parking",
+    coordinates: {
+      top: "0%",
+      left: "0%",
+      width: "18.5%",
+      height: "13%",
+    },
+  },
+  {
+    id: 3,
+    name: "Allez en prison!",
+    coordinates: {
+      top: "0%",
+      left: "122%",
+      width: "18.5%",
+      height: "13%",
+    },
+  },
+]
+
+export const districtZone: DistrictZone[] = [
   {
     id: 0,
     name: "Départ",
--- src/components/board/types.ts
+++ src/components/board/types.ts
@@ -11,7 +11,17 @@ export type CornerZone = {
 
 export type MainZone = {
   id: number
+  coordinates: {
+    top: string
+    left: string
+    width: string
+    height: string
+  }
+}
 
+export type DistrictZone = {
+  id: number
+  name: string
   coordinates: {
     top: string
     left: string
