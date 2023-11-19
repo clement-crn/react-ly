7a313e9e826dfb8ae692539f2f5892c3d1e7c1ea
Clément <cironclement.pro@gmail.com>
2023-11-19 20:20:19 +0100

    new positions

--- src/components/board/const.tsx
+++ src/components/board/const.tsx
@@ -68,8 +68,8 @@ export const districtZone: DistrictZone[] = [
     id: 2,
     name: "Rue ",
     coordinates: {
-      top: "0%",
-      left: "0%",
+      top: "86.5%",
+      left: "87%",
       width: "12%",
       height: "13%",
     },
@@ -78,9 +78,59 @@ export const districtZone: DistrictZone[] = [
     id: 3,
     name: "Rue ",
     coordinates: {
-      top: "0%",
-      left: "122%",
-      width: "12%",
+      top: "86.5%",
+      left: "75.5%",
+      width: "11.5%",
+      height: "13%",
+    },
+  },
+  {
+    id: 4,
+    name: "Rue ",
+    coordinates: {
+      top: "86.5%",
+      left: "64.5%",
+      width: "11.5%",
+      height: "13%",
+    },
+  },
+  {
+    id: 5,
+    name: "Rue ",
+    coordinates: {
+      top: "86.5%",
+      left: "52.5%",
+      width: "11.5%",
+      height: "13%",
+    },
+  },
+  {
+    id: 6,
+    name: "Rue ",
+    coordinates: {
+      top: "86.5%",
+      left: "41.5%",
+      width: "11.5%",
+      height: "13%",
+    },
+  },
+  {
+    id: 7,
+    name: "Rue ",
+    coordinates: {
+      top: "86.5%",
+      left: "30%",
+      width: "11.5%",
+      height: "13%",
+    },
+  },
+  {
+    id: 8,
+    name: "Rue ",
+    coordinates: {
+      top: "86.5%",
+      left: "18.5%",
+      width: "11.5%",
       height: "13%",
     },
   },
