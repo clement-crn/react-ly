86fdec7671bbf9e6e2fd456f6f8ebcdb1bc81c7e
Clément <cironclement.pro@gmail.com>
2023-11-19 20:19:38 +0100

    init rightmenu

--- src/components/right_menu/RightMenu.tsx
+++ src/components/right_menu/RightMenu.tsx
@@ -1,7 +1,14 @@
 import style from "./RightMenu.module.css"
 
 function RightMenu() {
-  return <div className={style.rightmenu_container}>RightMenu</div>
+  return (
+    <div className={style.rightmenu_container}>
+      <div className={style.rightmenu_wrapper}>
+        <div className={style.playerInfos}></div>
+        <div className={style.empty}></div>
+      </div>
+    </div>
+  )
 }
 
 export default RightMenu
