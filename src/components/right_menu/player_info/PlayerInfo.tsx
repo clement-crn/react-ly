f27eb91fce72887087bd55dfe733e0c197bf47e6
Clément <cironclement.pro@gmail.com>
2023-11-29 22:35:38 +0100

    upgrade PlayerInfos

--- src/components/right_menu/player_info/PlayerInfo.tsx
+++ src/components/right_menu/player_info/PlayerInfo.tsx
@@ -1,26 +1,34 @@
 import React from "react"
 import style from "./PlayerInfo.module.css"
-import { user_img } from "@/assets"
 
-const PlayerInfo = () => {
+import { PlayerPicture } from "../types"
+
+interface PlayerInfoProps {
+  playerName: string
+  bankMoney: number
+  playerPicture: PlayerPicture
+}
+
+const PlayerInfo = ({
+  playerName,
+  bankMoney,
+  playerPicture,
+}: PlayerInfoProps) => {
   return (
-    <>
-      <div className={style.playerinfo_container}>
-        <div className={style.playerinfo_wrapper}>
-          <img
-            src={user_img}
-            alt="Portrait du joueur."
-            className={style.user_img}
-          />
-          <div className={style.playerinfo_name_and_money_wrapper}>
-            <div className={style.playerinfo_name}>playerinfo_name</div>
-            <div className={style.playerinfo_bank_money}>
-              playerinfo_bank_money
-            </div>
-          </div>
+    <div className={style.playerinfo_container}>
+      <div className={style.playerinfo_wrapper}>
+        <img
+          src={playerPicture}
+          alt="Portrait du joueur."
+          className={style.user_img}
+        />
+        <div className={style.playerinfo_name_and_money_wrapper}>
+          <div className={style.playerinfo_name}>{playerName}</div>
+          <div className={style.playerinfo_bank_money}>{`${bankMoney} $`}</div>
+          {playerPicture}
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
