e8bf24d4faafee5ab6bf31919a5729aeeb1548bd
Clément <clement@waven-consulting.fr>
2024-05-05 17:30:13 +0200

    optimized DiceController

--- src/hooks/dice_controller/DiceController.tsx
+++ src/hooks/dice_controller/DiceController.tsx
@@ -2,38 +2,24 @@ import { useMainContext } from "@/context/context"
 import { useEffect } from "react"
 
 export const useDiceController = () => {
-  const {
-    currentPlayerIndex,
-    allPlayers,
-    currentPlayerPlaying,
-    setPlayer,
-    setCurrentPlayerPlaying,
-  } = useMainContext()
+  const { currentPlayerIndex, allPlayers, setPlayer, setCurrentPlayerPlaying } =
+    useMainContext()
   const player = allPlayers?.[currentPlayerIndex]
 
   const rollDicesMainGame = () => {
     const randomNumber = Math.floor(Math.random() * 12) + 1
-    if (player) {
-      setCurrentPlayerPlaying(player.id)
-      if (randomNumber + player?.boardPosition > 39) {
-        setPlayer(player.id, {
-          ...player,
-          boardPosition: randomNumber + player.boardPosition - 40,
-        })
-      } else {
-        setPlayer(player.id, {
-          ...player,
-          boardPosition: randomNumber + player.boardPosition,
-        })
-      }
-    }
+    if (!player) return
+    setCurrentPlayerPlaying(player.id)
+    const newPosition = (randomNumber + player.boardPosition) % 40
+    setPlayer(player.id, { ...player, boardPosition: newPosition })
   }
+
   useEffect(() => {
     if (player) {
       console.log(">>>Le joueur actuel est: ", player.username)
-      console.log(">>>Sa position est maintenant: ", player?.boardPosition)
+      console.log(">>>Sa position est maintenant: ", player.boardPosition)
     }
-  }, [currentPlayerPlaying])
+  }, [player, setCurrentPlayerPlaying, setPlayer])
 
   return { rollDicesMainGame }
 }
