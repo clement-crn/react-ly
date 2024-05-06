9482c2d3d6a1f368fec648b7a56cafc7bd701c81
Clément <clement@waven-consulting.fr>
2024-05-06 23:34:43 +0200

    jail feature test button

--- src/components/right_menu/main_buttons/MainButtons.tsx
+++ src/components/right_menu/main_buttons/MainButtons.tsx
@@ -4,15 +4,19 @@ import { useMainContext } from "@/context/context"
 import style from "./MainButtons.module.css"
 import { useDiceController } from "@/hooks/dice_controller/DiceController"
 import { useEffect } from "react"
+import { useJail } from "@/hooks/jail/Jail"
 
 function MainButtons() {
   const {
     startingOrder,
-    currentPlayerIndex,
+    currentPlayerPlaying,
+    allPlayers,
     setCurrentPlayerIndex,
     setCurrentPlayerPlaying,
   } = useMainContext()
   const { rollDicesMainGame } = useDiceController()
+  const { sendPlayerToJail } = useJail()
+  const player = allPlayers?.[currentPlayerPlaying]
 
   useEffect(() => {
     //humain commence
@@ -27,14 +31,25 @@ function MainButtons() {
   }
 
   const endTurn = () => {
-    const nextPlayerIndex = (currentPlayerIndex + 1) % startingOrder.length
+    const nextPlayerIndex = (currentPlayerPlaying + 1) % startingOrder.length
     setCurrentPlayerIndex(nextPlayerIndex)
   }
+
+  useEffect(() => {
+    console.log(">>>Le joueur est en prison: ", player?.isPrisoner)
+  }, [player?.isPrisoner])
 
   return (
     <div className={style.buttons_container}>
       <button onClick={() => startPlayerTurn()}>Lancer le dé</button>
       <button onClick={() => endTurn()}>joueur suivant</button>
+      {/* DEV */}
+      <button onClick={() => sendPlayerToJail()}>Prison</button>
+      player: {player?.username}
+      <br />
+      jail: {player?.isPrisoner ? "true" : "false"}
+      <br />
+      position: {player?.boardPosition}
     </div>
   )
 }
