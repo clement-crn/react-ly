74a5b1fce0b5234087b9f012f891a80e13dfbeeb
Clément <clement@waven-consulting.fr>
2024-05-07 22:58:09 +0200

    luck/add card modal

--- src/hooks/luck/Luck.tsx
+++ src/hooks/luck/Luck.tsx
@@ -1,13 +1,17 @@
+import React, { useState } from "react"
 import { luckCards } from "@/assets/cards/luck/consts"
 import { useJail } from "../jail/Jail"
 
 export const useLuck = () => {
   const { sendPlayerToJail } = useJail()
+  const [showModal, setShowModal] = useState(false)
+  const [cardText, setCardText] = useState<string | undefined>("")
 
   const pickLuckCard = () => {
     console.log("Pick luck card")
     const randomIndex = Math.floor(Math.random() * luckCards.length)
     const action = luckCards[randomIndex]?.action
+    const description = luckCards[randomIndex]?.description
 
     switch (action) {
       case "GOTOJAIL":
@@ -17,9 +21,28 @@ export const useLuck = () => {
       // case "ADVANCETOVILLETTE":
       //   return useAdvanceToIllinoisAvenue();
       default:
-        return null
+        setCardText(description)
+        setShowModal(true)
     }
   }
 
-  return { pickLuckCard }
+  const closeModal = () => {
+    setShowModal(false)
+  }
+
+  const Modal = ({ text, onClose }) => {
+    return (
+      <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={onClose}>
+            &times;
+          </span>
+          <h3>Carte chance!</h3>
+          <p>{text}</p>
+        </div>
+      </div>
+    )
+  }
+
+  return { pickLuckCard, showModal, cardText, closeModal, Modal }
 }
