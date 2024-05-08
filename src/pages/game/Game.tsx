1f9d52649098f015af33d586d997fb6e844fbaf9
Clément <clement@waven-consulting.fr>
2024-05-08 22:20:35 +0200

    UI/better back arrow

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -3,6 +3,7 @@ import style from "./game.module.css"
 import { useMainContext } from "../../context/context"
 import RightMenu from "@/components/right_menu/RightMenu"
 import { Board } from "@/components/board"
+import { IoArrowBack } from "react-icons/io5"
 
 function Game() {
   const { setIsGameStarted } = useMainContext()
@@ -15,7 +16,7 @@ function Game() {
     <>
       <div className={style.game_container}>
         <button className={style.back_to_landingpage} onClick={back}>
-          retour
+          <IoArrowBack />
         </button>
         <div className={style.rightmenu_wrapper}>
           <RightMenu />
