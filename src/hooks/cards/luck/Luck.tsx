37c67812fff10f90c0580ca6d768efd1389d229b
Clément <clement@waven-consulting.fr>
2024-05-08 17:28:17 +0200

    luck card/tax/handle case if player goes bankrupt

--- src/hooks/cards/luck/Luck.tsx
+++ src/hooks/cards/luck/Luck.tsx
@@ -33,21 +33,26 @@ export const useLuck = () => {
     console.log(">>>>payTax called from useLuck hook")
     if (!player) return
 
+    if (player.money - 200 < 0) {
+      console.log("Player has not enough money to pay tax")
+      setPlayer(currentPlayerPlaying, {
+        ...player,
+        isBankrupt: true,
+      })
+      return
+    }
+
     setPlayer(currentPlayerPlaying, {
       ...player,
       money: player.money - 200,
     })
   }
 
-  const closeModal = () => {
-    setShowModal(false)
-  }
-
-  const Modal = ({ text, onClose }) => {
+  const Modal = ({ text }) => {
     return (
       <div className="modal">
         <div className="modal-content">
-          <span className="close" onClick={onClose}>
+          <span className="close" onClick={() => setShowModal(false)}>
             &times;
           </span>
           <h3>Carte chance!</h3>
@@ -57,5 +62,5 @@ export const useLuck = () => {
     )
   }
 
-  return { pickLuckCard, showModal, cardText, closeModal, Modal }
+  return { pickLuckCard, showModal, cardText, Modal, payTax }
 }
