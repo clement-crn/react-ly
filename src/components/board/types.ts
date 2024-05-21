767788c61f18dbcc7f135ce4ffb71be44f541a6a
Clément <clement@waven-consulting.fr>
2024-05-21 22:03:48 +0200

    board/new case types

--- src/components/board/types.ts
+++ src/components/board/types.ts
@@ -9,10 +9,63 @@ export type CornerZone = {
   }
 }
 
+export type Chest = {
+  id: number
+  name: string
+  coordinates: {
+    top: string
+    left: string
+    width: string
+    height: string
+  }
+}
+export type TaxZone = {
+  id: number
+  name: string
+  coordinates: {
+    top: string
+    left: string
+    width: string
+    height: string
+  }
+}
+
+export type CyberCafe = {
+  id: number
+  name: string
+  coordinates: {
+    top: string
+    left: string
+    width: string
+    height: string
+  }
+}
+
+export type LuckZone = {
+  id: number
+  name: string
+  coordinates: {
+    top: string
+    left: string
+    width: string
+    height: string
+  }
+}
+
 export type DistrictZone = {
   id: number
   name: string
   price: number
+  color:
+    | "light_blue"
+    | "dark_blue"
+    | "green"
+    | "yellow"
+    | "red"
+    | "purple"
+    | "orange"
+    | "pink"
+    | "brown"
   coordinates: {
     top: string
     left: string
